fca9e0f69206d252a0ed1815f6889adc8ba47448
unknown <aminefrira@gmail.com>
2023-05-17 23:58:45 +0100

    first commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,17 @@
+import "./header.css";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <div className="headerTitles">
+        <span className="headerTitleSm">Find the best</span>
+        <span className="headerTitleLg">RESTAURANT</span>
+      </div>
+      <img
+        className="headerImg"
+        src="images/pexels-photo-2961968.jpeg"
+        alt=""
+      />
+    </div>
+  );
+}
